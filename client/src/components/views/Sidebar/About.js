7a226b13a1c3d90950a3471d02f4a438da5091bf
sukha <sukha@sukhas-Air.fritz.box>
2021-02-19 20:24:53 +0100

    puppeteer-heroku-buildpack

--- client/src/components/views/Sidebar/About.js
+++ client/src/components/views/Sidebar/About.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Carousel} from 'react-bootstrap';
+
+
 
 export default function About() {
     return (
         <div>
-            About
+
         </div>
     )
 }
